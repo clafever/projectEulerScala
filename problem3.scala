fb067fa498712be93a75416f6fddc9b768c7bfc8
Chad LaFever <clafever@gmail.com>
2016-12-20 20:29:07 -0600

    Update problem3.scala
    killed some whitespace

--- problem3.scala
+++ problem3.scala
@@ -1,8 +1,6 @@
 // The prime factors of 13195 are 5, 7, 13 and 29.
 // What is the largest prime factor of the number 600851475143 ?
 
-
-
 // take an argument from the command line scala problem3.scala 600851475143
 var test_int = BigInt(args(0))
 
