653b4471e9e68a7773a88ec5a4df343757b5b90f
Chad LaFever <clafever@gmail.com>
2016-12-22 12:17:44 -0600

    fixing dat space

--- problem1.scala
+++ problem1.scala
@@ -5,7 +5,7 @@
 var list = (1 until 1000)
 
 // filter them such that 3 OR 5 is a divisor
-var subset = list.filter(n => (n % 3 == 0 || n % 5 ==0 ))
+var subset = list.filter(n => (n % 3 == 0 || n % 5 == 0 ))
 
 // add them up
 var result = subset.sum
